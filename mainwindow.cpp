713da6d7b3e2187eb1c88542db9295ee9a8fdaf2
Andrew campbell <andrew@OrangeBeast.(none)>
2013-02-09 17:40:18 +1300

    acgit: Clear comments and fix up treeview headers and extra sections

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -26,11 +26,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    //set column count to 4
-    // TODO make change back to 4
-    //ui->revList->setColumnCount(5);
-
 }
 
 MainWindow::~MainWindow()
@@ -39,49 +34,64 @@ MainWindow::~MainWindow()
 }
 
 /**
- * @brief MainWindow::revWalk walk the full repo
- * TODO make this less static
+ * @brief MainWindow::revWalk display all of the commits in the repo
  */
 void MainWindow::revWalk ()
 {
-//    if (repo == NULL)
-//    {
-//        QMessageBox msgBox;
-//        msgBox.setText("No git repository selected");
-//        msgBox.setInformativeText("Please open one!");
-//        msgBox.exec();
+    QStandardItemModel *model = new QStandardItemModel(this);
+    model->setColumnCount(4);
 
-//        return;
-//    }
+    // setup header info
+    QStandardItem *header = new QStandardItem();
+    header->setData("Rev", Qt::DisplayRole);
+    model->setHorizontalHeaderItem(0, header);
 
-//    while (walker.next(walkCommit) == true)
-//    {
-//        QTreeWidgetItem *commit = new QTreeWidgetItem(ui->revList);
+    // setup header info
+    header = new QStandardItem();
+    header->setData("Short Log", Qt::DisplayRole);
+    model->setHorizontalHeaderItem(1, header);
 
-//        // set short message
-//        commit->setText(1, walkCommit.shortMessage());
+    // setup author header info
+    header = new QStandardItem();
+    header->setData("Author", Qt::DisplayRole);
+    model->setHorizontalHeaderItem(2, header);
 
-//        // set author
-//        commit->setText(2, walkCommit.author().name());
+    //setup when author made commit header
+    header = new QStandardItem();
+    header->setData("Date", Qt::DisplayRole);
+    model->setHorizontalHeaderItem(3, header);
 
-//        //set author date
-//        commit->setText(3, walkCommit.author().when().toString());
+    // loop over all commits and set up all useful info in the
+    // rev tree view
+    int row = 0;
+    foreach (Commit* commit, repo->getAllCommits())
+    {
+        QStandardItem *index0 = new QStandardItem();
+        index0->setData("", Qt::DisplayRole);
+        model->setItem(row, 0, index0);
 
-//        //TODO remove me - sets branch name
-////        LibQGit2::QGitRef *ref = new LibQGit2::QGitRef ();
+        QStandardItem *index1 = new QStandardItem();
+        index1->setData(commit->getCommit().shortMessage(), Qt::DisplayRole);
+        model->setItem(row, 1, index1);
 
-////        try
-////        {
-////            ref->setOId(walkCommit.oid());
-////        }
-////        catch (...)
-////        {
-////            qDebug() << "An expection occured";
-////        }
+        QStandardItem *index2 = new QStandardItem();
+        index2->setData(commit->getCommit().author().name(), Qt::DisplayRole);
+        model->setItem(row, 2, index2);
 
-////        commit->setText(4, ref->name());
-//    }
+        QStandardItem *index3 = new QStandardItem();
+        index3->setData(commit->getCommit().author().when().toString(), Qt::DisplayRole);
+        model->setItem(row, 3, index3);
 
+        row++;
+    }
+    ui->revList->setModel(model);
+
+    revViewDelegate *delegate = new revViewDelegate(repo, this);
+    ui->revList->setItemDelegate (delegate);
+
+    // resize short comment column so more text can be seen
+    ui->revList->setColumnWidth(1, 350);
+    ui->revList->show();
 }
 
 void MainWindow::loadRepo()
@@ -91,118 +101,6 @@ void MainWindow::loadRepo()
 
 void MainWindow::findAllBranches()
 {
-
-    QStandardItemModel *model = new QStandardItemModel(this);
-    model->setColumnCount(5);
-
-    qDebug() << "got to here";
-    int row = 0;
-    foreach (Commit* commit, repo->getAllCommits())
-    {
-        qDebug() << row;
-        QStandardItem *index0 = new QStandardItem();
-        index0->setData("", Qt::DisplayRole);
-        model->setItem(row, 0, index0);
-
-        QStandardItem *index1 = new QStandardItem();
-        index1->setData(commit->getCommit().shortMessage(), Qt::DisplayRole);
-        model->setItem(row, 1, index1);
-
-//        QStandardItem *index2 = new QStandardItem(commit->getCommit().author().name());
-//        model.setItem(row, 2, index2);
-
-//        QStandardItem *index3 = new QStandardItem(commit->getCommit().author().when().toString());
-//        model.setItem(row, 3, index3);
-
-//        QStandardItem *index0 = new QStandardItem();
-//        model.setItem(row, 0, index0);
-//        QModelIndex index1 = model.index(row, 1, QModelIndex());
-//        model.setData(index1, QVariant(commit->getCommit().shortMessage()));
-//        QModelIndex index2 = model.index(row, 2, QModelIndex());
-//        model.setData(index2, QVariant(commit->getCommit().author().name()));
-//        QModelIndex index3 = model.index(row, 3, QModelIndex());
-//        model.setData(index3, QVariant(commit->getCommit().author().when().toString()));
-
-//        model.insertRow(row, model);
-        row++;
-    }
-    ui->revList->setModel(model);
-
-    revViewDelegate *delegate = new revViewDelegate(repo, this);
-    ui->revList->setItemDelegate (delegate);
-    ui->revList->show();
-
-//    tableView.setWindowTitle(QObject::tr("Spin Box Delegate"));
-//    tableView.show();
-
-//    QVector<Commit*> commits = repo->getAllCommits();
-//    QList<QTreeWidgetItem*> rowList;
-//    QTreeWidgetItem *treeCommit;
-
-//    foreach (Commit* commit, commits)
-//    {
-//        //qDebug() << QString::number(commit->getCommitType()) << " " << commit->getRow() << " " << commit->getCommit().shortMessage();
-
-//        if (commit->getRow() == 0)
-//        {
-//            if (rowList.isEmpty() == false)
-//                rowList.removeLast();
-//            treeCommit = new QTreeWidgetItem(ui->revList);
-//            rowList.append(treeCommit);
-//        }
-//        else
-//        {
-
-//            if (commit->getCommitType() == Commit::BRANCH_COMMIT)
-//            {
-//                if (rowList.isEmpty() == false)
-//                    rowList.removeLast();
-//                treeCommit = new QTreeWidgetItem(rowList.at(rowList.count() - 1));
-//            }
-
-//            if (commit->getCommitType() == Commit::BRANCH_MERGE_COMMIT)
-//            {
-//                treeCommit = new QTreeWidgetItem(rowList.at(rowList.count() - 1));
-//                if (rowList.isEmpty() == false)
-//                    rowList.removeLast();
-//                rowList.append (treeCommit);
-//            }
-
-//            if (commit->getCommitType() == Commit::MERGE_COMMIT)
-//            {
-//                treeCommit = new QTreeWidgetItem(rowList.at(rowList.count() - 1));
-//                rowList.append (treeCommit);
-//            }
-
-//            if (commit->getCommitType() == Commit::NORMAL_COMMIT)
-//            {
-//                if (rowList.isEmpty() == false && rowList.count() > commit->getRow())
-//                {
-//                    treeCommit = new QTreeWidgetItem(rowList.at(commit->getRow() - 1));
-//                    rowList.removeAt(commit->getRow() - 1);
-//                    rowList.insert(commit->getRow()- 1, treeCommit);
-//                }
-//                else if (rowList.isEmpty() == false)
-//                {
-//                    treeCommit = new QTreeWidgetItem(rowList.at(rowList.count() -1));
-//                    rowList.removeLast();
-//                    rowList.append(treeCommit);
-//                }
-//            }
-//        }
-
-//        // set short message
-//        treeCommit->setText(1, QString::number(commit->getRow()) + commit->getCommit().shortMessage());
-
-//        // set author
-//        treeCommit->setText(2, commit->getCommit().author().name());
-
-//        //set author date
-//        treeCommit->setText(3, commit->getCommit().author().when().toString());
-
-//        treeCommit->setExpanded(true);
-
-//    }
 
 }
 
@@ -214,7 +112,7 @@ void MainWindow::on_actionOpen_triggered()
     repo = new acRepo(folderName + "/.git");
 
     connect (repo, SIGNAL(repoOpened()),this, SLOT(loadRepo()));
-   // revWalk();
+    revWalk();
     findAllBranches();
 }
 
